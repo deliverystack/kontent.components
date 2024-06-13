b6fe367ea093410da052915f97e4f788c8308d2f
John West <github@deliverystack.net>
2024-06-12 17:45:03 -0700

    restore lost file, set title,

--- src/components/UseCmsItem.jsx
+++ src/components/UseCmsItem.jsx
@@ -2,8 +2,11 @@ import React, { useEffect } from 'react';
 
 const UseCmsEntry = () => {
     useEffect(() => {
-        if (window.cmsPageItem.item !== undefined)
+        if (window.cmsPageItem.item !== undefined) {
+            document.title = window.cmsPageItem.item.elements.commoncontent__title.value;
             console.log(window.cmsPageItem);
+        }
+
       }, [])
 
     return(<></>)
